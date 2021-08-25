cf605d72f1d057aefed2a3b3e543b9118c5c92d1
Eduardo Silva <educwb@gmail.com>
2021-08-25 02:37:22 -0300

    adicionando JSS ao componente Header

--- src/partials/Header/Header.js
+++ src/partials/Header/Header.js
@@ -3,18 +3,22 @@ import {
   Toolbar, 
   Typography, 
   Button, 
-  IconButton} from '@material-ui/core'
+  IconButton,
+} from '@material-ui/core'
+
 import MenuIcon from '@material-ui/icons/Menu'
+import useStyles from './Header.style'
 
 const Header = () => {
+  const classes = useStyles()
   return (
     <AppBar position="static">
       <Toolbar>
         <IconButton edge="start" color="inherit" aria-label="menu">
           <MenuIcon />
         </IconButton>
-        <Typography variant="h6">
-          News
+        <Typography variant="h6" className={classes.title}>
+          My App
         </Typography>
         <Button color="inherit">Login</Button>
       </Toolbar>
