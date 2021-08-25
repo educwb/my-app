405cd4d3cf3c58982abaff7e3780fe131a85cf5b
Eduardo Silva <educwb@gmail.com>
2021-08-25 11:37:33 -0300

    adicionando Template Default

---
+++ src/templates/Default.style.js
@@ -0,0 +1,12 @@
+import { cyan } from '@material-ui/core/colors'
+import { makeStyles } from '@material-ui/core/styles'
+
+// JSS
+const useStyles = makeStyles(() => ({
+  container: {
+    padding: '15px 0',
+    background: 'cyan'
+  }
+}))
+
+export default useStyles
