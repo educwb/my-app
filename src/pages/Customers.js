3c9301c905efd51b7e2e77130e60f35a0bee23d2
Eduardo Silva <educwb@gmail.com>
2021-08-25 19:01:55 -0300

    Usando Grid

--- src/pages/Customers.js
+++ src/pages/Customers.js
@@ -1,11 +1,24 @@
 import { useState, useEffect} from 'react'
+import { makeStyles } from '@material-ui/core/styles'
 import axios from 'axios'
+import Grid from '@material-ui/core/Grid'
+
 import CustomerCard from '../components/CustomerCard'
+import { grey } from '@material-ui/core/colors'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  card: {
+    margin: theme.spacing(2),
+    background: grey[300]
+  },
+}))
 
 const Customers = () => {
+  const classes = useStyles()
   const [customers, setCustomers] = useState([])
-
-  console.log(customers)
 
   useEffect(() =>{
     axios.get('https://reqres.in/api/users')
@@ -19,16 +32,21 @@ const Customers = () => {
   return (
     <>
       <h1>Customers</h1>
-      {
-        customers.map(item => (
-          <CustomerCard
-            first_name={item.first_name}
-            last_name={item.last_name}
-            email={item.email}
-            avatar={item.avatar}
-          />
-        ))
-      }
+      <Grid container>
+        {
+          customers.map(item => (
+            <Grid item xs={12} md={4}>
+              <CustomerCard
+                first_name={item.first_name}
+                last_name={item.last_name}
+                email={item.email}
+                avatar={item.avatar}
+                className={classes.card}
+              />
+            </Grid>
+          ))
+        }
+      </Grid>
     </>
   )
 }
