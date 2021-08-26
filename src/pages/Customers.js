1886ed700ccbf3d78da0613ba8d9dee37b8f7e3b
Eduardo Silva <educwb@gmail.com>
2021-08-26 14:27:13 -0300

    adicionando Deletar Usuario - Integracao com API

--- src/pages/Customers.js
+++ src/pages/Customers.js
@@ -29,17 +29,28 @@ const Customers = () => {
       })
   }, [])
 
+  const handleRemoveCustomer = id => {
+    axios.delete(`https://reqres.in/api/users/${id}`)
+      .then(() => {
+        const newCustomersState = customers.filter(customer => customer.id !== id)
+
+        setCustomers(newCustomersState)
+      })
+  }
+
   return (
     <Grid container>
       {
         customers.map(item => (
           <Grid item xs={12} md={4}>
             <CustomerCard
+              id={item.id}
               first_name={item.first_name}
               last_name={item.last_name}
               email={item.email}
               avatar={item.avatar}
               className={classes.card}
+              onRemoveCustomer={handleRemoveCustomer}
             />
           </Grid>
         ))
